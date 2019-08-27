e314bf947889a5ed51efbdb3daee2956a4a58557
GustavoGB <gustavo.gobetti98@gmail.com>
2019-08-27 07:59:58 -0700

    Analisando Codigo

--- aps1Hashi/geometry.c
+++ aps1Hashi/geometry.c
@@ -13,28 +13,28 @@ int verify(point p, point a, point b) {
     deltaX = diferenca;               // vertical
     if ((p.x >= a.x) && (p.x <= b.x)) // tem o ponto
     {
-      if (((p.y > a.y) && (p.y <= b.y)) || ((p.y > b.y) && (p.y <= a.y)))
+      if (((p.y > a.y) && (p.y <= b.y)) || ((p.y > b.y) && (p.y <= a.y))) // Bem a ponta da reta/  
         return 2;
     }
   }
-  double m = deltaY / deltaX;
-  //Caso 2
+  double m = deltaY / deltaX; // coeficiente angular da reta
+  
   if ((p.x >= MIN(a.x, b.x)) && (p.x <= MAX(a.x, b.x))) // limitando x
   {
 
-    if (fabs(p.x - a.x) <= diferenca && fabs(p.y - a.y) <= diferenca)
+    if (fabs(p.x - a.x) <= diferenca && fabs(p.y - a.y) <= diferenca) // reta no ponto comparar o ponto A 336 testes
       return 2;
-    if (fabs(p.x - b.x) <= diferenca && fabs(p.y - b.y) <= diferenca)
+    if (fabs(p.x - b.x) <= diferenca && fabs(p.y - b.y) <= diferenca) // reta no ponto comparar o ponto B 336 testes                               
       return 2;
-    if (fabs(p.y - (a.y + m * (p.x - a.x))) <= diferenca)
+    if (fabs(p.y - (a.y + m * (p.x - a.x))) <= diferenca) // Reta abaixo do ponto, 336 testes 
       return 2;
   }
   // Limitando o y
   if ((p.y > MIN(a.y, b.y)) && (p.y <= MAX(a.y, b.y))) {
-    if (p.x < MIN(a.x, b.x))
+    if (p.x < MIN(a.x, b.x)) // reta atras do ponto, nao cruzando 124 testes
       return 1;
-    if ((p.y - a.y < m * (p.x - a.x)) && (m <= 0))
-      return 1; // m negativo
+    if ((p.y - a.y < m * (p.x - a.x)) && (m <= 0)) 
+      return 1; // m negativo reta descrescente 
     if ((p.y - a.y > m * (p.x - a.x)) && (m >= 0))
       return 1; // m positivo
   }
